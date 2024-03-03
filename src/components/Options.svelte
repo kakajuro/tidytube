2d5b20a801e4db97aa6632378bfab40f9da9f751
swishyy <famodavid1@gmail.com>
2024-03-03 22:09:54 +0000

    Improved options menu + started implementing settings

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -5,6 +5,12 @@
 
   let darkMode;
 
+  let removeShortsFromSearchToggle = true;
+  let removeAdsFromSeachToggle = true;
+
+  const handleRemoveShortsFromSearchToggle = () => removeShortsFromSearchToggle = !removeShortsFromSearchToggle;
+  const handleRemoveAdsFromSearchToggle = () => removeAdsFromSeachToggle = !removeAdsFromSeachToggle;
+
   onMount(async () => {
     darkMode = await getDarkMode();
   });
@@ -13,4 +19,40 @@
 <main class="w-full h-screen p-4 flex flex-col items-center select-none font-jost" class:bg-custom-dark={darkMode}>
   <h1 class="font-semibold text-5xl mt-12 pb-4" class:text-white={darkMode}>Options</h1>
   <h2 class="font-semibold text-3xl mt-4 pb-4" class:text-white={darkMode}>Search</h2>
+  <div class="flex flex-col mb-6">
+    <div class="space-x-2">
+      <input
+        id="shortFromSearch" 
+        type="checkbox"
+        class="w-4 h-4 rounded-sm accent-[#FF0000]"
+        checked={removeShortsFromSearchToggle}
+        on:change={handleRemoveShortsFromSearchToggle}
+      />
+      <label 
+        for="shortsFromSearch"
+        class="text-xl" 
+        class:text-white={darkMode}>
+        Remove Shorts from search
+      </label>
+    </div>
+    <p class="font-bold" class:text-white={darkMode}>Stops Shorts from appearing in the search page</p>
+  </div>
+  <div class="flex flex-col mb-6">
+    <div class="space-x-2">
+      <input
+        id="adsFromSearch" 
+        type="checkbox"
+        class="w-4 h-4 rounded-sm accent-[#FF0000]"
+        checked={removeAdsFromSeachToggle}
+        on:change={handleRemoveAdsFromSearchToggle}
+      />
+      <label 
+        for="adsFromSearch"
+        class="text-xl" 
+        class:text-white={darkMode}>
+        Remove ads from search
+      </label>
+    </div>
+    <p class="font-bold" class:text-white={darkMode}>Stops ads from appearing in the search page</p>
+  </div>
 </main>
---
+++ src/util/settingsHandler.ts
@@ -0,0 +1,37 @@
+import { browser } from "webextension-polyfill-ts";
+
+let defaults = {
+  "removeShortFromSearch": true,
+  "removeAdsFromSearch": true
+}
+
+export async function getSettings() {
+  try {
+    let { settings } = await browser.storage.local.get("settings");
+
+    if (settings === undefined) {
+      // Set default settings if no value in storage
+      browser.storage.local.set({"settings": defaults})
+      .catch((error) => console.error(`Error setting default settings: ${error}`))
+    } else {
+      // Return stored value if there is one
+      return settings
+    }
+  } catch (error) {
+    console.error(`An error occurred trying to load extension settings: ${error}`);
+    return defaults
+  }
+}
+
+export async function setSettings(data:object) {
+  let { settings } = await browser.storage.local.get("settings");
+  let newSettings = {...settings, ...data}
+
+  browser.storage.local.set({"settings": newSettings})
+  .catch((error) => console.error(`An error occured when changing settings : ${error}`))
+}
+
+export async function clearSettings() {
+  browser.storage.local.remove("settings")
+  .catch((error) => console.error(`An error occurred removing settings from storage: ${error}`))
+}
