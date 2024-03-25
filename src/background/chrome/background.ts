ab553ae6c569adfed9d65af01fa713f7000df442
swishyy <famodavid1@gmail.com>
2024-03-25 21:39:50 +0000

    Runs ext earlier through bg-script (all platforms)

--- src/background/chrome/background.ts
+++ src/background/chrome/background.ts
@@ -1,5 +1,7 @@
 /// <reference types="chrome"/>
 
+import { browser } from "webextension-polyfill-ts";
+
 chrome.runtime.onInstalled.addListener(function () {
   // Make extension work on all pages
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -10,4 +12,16 @@ chrome.runtime.onInstalled.addListener(function () {
       },
     ]);
   });
+});
+
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == "loading") {    
+    try {
+      browser.tabs.sendMessage(tabId, "tidyWhileLoading");
+      setTimeout(null, 1000);
+      browser.tabs.sendMessage(tabId, "tidyWhileLoading");
+    } catch (err) {
+      null;
+    }
+  }
 });
