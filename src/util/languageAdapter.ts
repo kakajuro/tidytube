9822b84ec517090f7e8c7ce9c5ff8bcdb3d50d73
swishyy <famodavid1@gmail.com>
2024-08-25 14:32:38 +0100

    Added support for french for all UI

--- src/util/languageAdapter.ts
+++ src/util/languageAdapter.ts
@@ -15,12 +15,12 @@ interface LanguageAdapter {
 export const AdapterFR:LanguageAdapter = {
   ChannelsNewToYou: ["Chaînes que vous avez manquées"],
   LatestPostsFrom: ["Derniers posts de", "Dernières posts de"],
-  LatestFrom: ["Dernières de", "Dierniers de"],
+  LatestFrom: ["Dernières vidéos de", "Dierniers vidéos de"],
   ForYou: ["Pour vous"],
   FromRelatedSearches: ["Résultats des recherches associées"],
   PeopleAlsoSearchFor: ["Recherches similaires"],
   PeopleAlsoWatched: ["Les internautes ont aussi regardé"],
   PreviouslyWatched: ["Regardées précédemment"],
   ShortsRemixingThisVideo: ["Shorts utilisant l'audio de cette vidéo"],
-  news: ["news" || "nouvelles"]
+  news: ["news", "nouvelles"]
 }
