51f166735aed9428c77d18ea57bc5bcd6a878b15
swishyy <famodavid1@gmail.com>
2024-09-02 20:31:04 +0100

    Edited build script (unzipped dirs are removed)

--- packageExtensions.js
+++ packageExtensions.js
@@ -1,60 +1,81 @@
 const fs = require("fs");
 const path = require("path");
-const util = require("util");
 const { rimrafSync } = require("rimraf");
 const { execSync } = require("child_process");
 const zipdir = require("zip-dir");
-
-const execPromise = util.promisify(execSync)
 
 let outputDir = path.join(__dirname, "dist");
 
 let platforms = ["chrome", "firefox", "edge"];
 
-// Empty existing dist directory if there is one
-if (fs.existsSync(outputDir)) {
-  try {
-    rimrafSync(outputDir);
-    console.log("Removed existing output directory sucessfully");
-  } catch (error) {
-    console.error("Error removing existing output directory: ${error}")
+async function packageExtension() {
+  // Empty existing dist directory if there is one
+  if (fs.existsSync(outputDir)) {
+    try {
+      rimrafSync(outputDir);
+      console.log("Removed existing output directory sucessfully");
+    } catch (error) {
+      console.error("Error removing existing output directory: ${error}")
+    }
   }
+
+  fs.mkdirSync(outputDir);
+  console.log("dist directory created");
+
+  // Build for each platform
+  for (let platform of platforms) {
+
+    console.log(`Building for ${platform.toUpperCase()}...`)
+
+    try {
+      await execSync(`yarn app:${platform}`);
+    } catch (error) {
+      console.error(`Error building for ${platform.toUpperCase()}: ${error}`);
+    }
+
+    console.log(`Built for ${platform.toUpperCase()} sucessfully!`);
+
+  }
+
+  // Zip
+  for (let platform of platforms) {
+    console.log(`Attempting to zip: ${platform.toUpperCase()}...`);
+
+    let inPath = path.join(__dirname, `/dist/${platform}_dist`);
+    let outPath = path.join(__dirname, `/dist/${platform}_dist.zip`);
+
+    try {
+      await new Promise((resolve, reject) => {
+        zipdir(inPath,
+          {
+            each: (filePath) =>
+              console.log(
+                filePath.replace(/^.*[\\/]/, ""),
+                `added to ${platform.toUpperCase()} zip`
+              ),
+            saveTo: outPath,
+          },
+          (err) => {
+            if (err) {
+              reject(`An error occurred while zipping: ${err}`);
+            } else {
+              console.log(`Zipped: ${platform.toUpperCase()}`);
+              resolve();
+            }
+          }
+        );
+      });
+
+    // Remove unzipped builds
+    rimrafSync(inPath);
+    } catch (error) {
+      console.error(`Error zipping for ${platform.toUpperCase()}: ${error}`);
+    }
+
+  }
+
+  console.log("Finished building.")
+
 }
 
-fs.mkdirSync(outputDir);
-console.log("dist directory created");
-
-// Build for each platform
-for (let platform of platforms) {
-
-  console.log(`Building for ${platform.toUpperCase()}...`)
-
-  execPromise(`yarn app:${platform}`)
-  .then(() => console.log(`Built for ${platform.toUpperCase()} sucessfully!`))
-  .catch(error => console.error(`Error building for ${platform.toUpperCase()}: ${error}`))
-
-  console.log(`Built for ${platform.toUpperCase()} sucessfully!`);
-
-}
-
-// Zip
-for (let platform of platforms) {
-
-  console.log(`Attempting to zip: ${platform.toUpperCase()}...`);
-
-  let inPath = path.join(__dirname, `/dist/${platform}_dist`);
-  let outPath = path.join(__dirname, `/dist/${platform}_dist.zip`);
-
-  zipdir(inPath, 
-    { each: path => console.log(path.replace(/^.*[\\/]/, ''), `added to ${platform.toUpperCase()} zip`), saveTo: outPath },  
-    (err, buffer) => {
-      if (err) {
-        console.warn("An error occurred while zipping: " + err);
-      } else {
-        console.log(`Zipped: ${platform.toUpperCase()}`);
-      }
-  })
-
-}
-
-console.log("Finished building.")
+packageExtension();
